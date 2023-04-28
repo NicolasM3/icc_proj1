2f9417beee0eb937855f01a45974426ec819e3eb
nicolas.oliveira <nicolas.oliveira@ifood.com.br>
2023-04-27 21:09:53 -0300

    feat: add readUser and removeUser

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ struct User{
     float amount;
 };
 
+struct UserArray {
+    struct User *users;
+    int size;
+};
+
 void writeNewLine(char filename[], char line[]) {
     FILE *file = fopen(filename, "a");
 
@@ -21,14 +26,14 @@ void writeNewLine(char filename[], char line[]) {
     fclose(file);
 }
 
-struct User * readUsersFromFile(char filename[]) {
+struct UserArray readUsersFromFile(char filename[]) {
     FILE *file = fopen(filename, "r");
     int size = 0;
     struct User *users = NULL;
 
     if (file == NULL) {
         printf("Error opening file: %s\n", filename);
-        return 0;
+        return (struct UserArray){NULL, 0};
     }
 
     while (!feof(file)) {
@@ -42,19 +47,96 @@ struct User * readUsersFromFile(char filename[]) {
     }
 
     fclose(file);
-    return users;
+    return (struct UserArray){users, size};
+}
+
+struct User readUser(char filename[], int id) {
+    FILE *file = fopen(filename, "r");
+    struct User user;
+
+    if (file == NULL) {
+        printf("Error opening file: %s\n", filename);
+        return user;
+    }
+
+    while (!feof(file)) {
+        fscanf(file,"%d %s %f", &user.id, user.name, &user.amount);
+
+        if (user.id == id) {
+            fclose(file);
+            return user;
+        }
+    }
+
+    fclose(file);
+    return user;
+}
+
+// Esse metodo esta duplicando a ultima linha do arquivo
+void removeUser(char filename[], int id) {
+    FILE *file = fopen(filename, "r");
+    FILE *tempFile = fopen("temp.txt", "w");
+    struct User user;
+
+    if (file == NULL) {
+        printf("Error opening file: %s\n", filename);
+        return;
+    }
+
+    while (!feof(file)) {
+        fscanf(file,"%d %s %f", &user.id, user.name, &user.amount);
+
+        if (user.id == id) {
+            continue;
+        }
+
+        fprintf(tempFile, "%d %s %f\n", user.id, user.name, user.amount);
+    }
+
+    fclose(file);
+    fclose(tempFile);
+
+    remove(filename);
+    rename("temp.txt", filename);
 }
 
 int main() {
     char filename[] = "File1.txt";
 
+    printf("ESCREVENDO\n");
+
     writeNewLine(filename, "1 Nicolas 1000.0");
     writeNewLine(filename, "2 Romanhole 1200.0");
     writeNewLine(filename, "3 Prato 2200.0");
+    writeNewLine(filename, "4 Jaime 3200.0");
+    
+    printf("LENDO TODOS OS USUARIOS\n");
 
-    struct User *users = readUsersFromFile("File1.txt");
+    struct UserArray usersResponse = readUsersFromFile(filename);
+    struct User *users = usersResponse.users;
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < usersResponse.size - 1; i++) {
         printf("%d %s %f\n", users[i].id, users[i].name, users[i].amount);
     }
+
+    printf("LENDO UM USUARIO\n");
+
+    struct User user = readUser(filename, 2);
+
+    printf("%d %s %f\n", user.id, user.name, user.amount);
+
+    printf("REMOVENDO UM USUARIO\n");
+
+    removeUser(filename, 2);
+
+    usersResponse = readUsersFromFile(filename);
+    users = usersResponse.users;
+
+    for (int i = 0; i < usersResponse.size - 1; i++) {
+        printf("%d %s %f\n", users[i].id, users[i].name, users[i].amount);
+    }
+    
+    free(users);
+
+    return 0;
 }
